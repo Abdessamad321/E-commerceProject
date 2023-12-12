fa4b16ff28dc2aac3f074021965ae87acb42c300
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-12-12 16:07:22 +0100

    oldygoldyhouse

--- client/src/Pages/Profile/profile.jsx
+++ client/src/Pages/Profile/profile.jsx
@@ -1,208 +1,7 @@
-// import React, { useState, useEffect, useContext } from "react";
-// import axios from "axios";
-// import Button from "@mui/material/Button";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { AuthContext } from "../../../../admin/src/AuthContext";
-// import "./Profile.css";
-
-// function Profil() {
-//   const authContext = useContext(AuthContext);
-//   const [user, setUser] = useState({
-//     first_name: "",
-//     last_name: "",
-//     email: "",
-//     password: "",
-//     confirmPassword: "",
-//     customer_image: null,
-//   });
-
-//   const [customerId, setCustomerId] = useState("");
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const storedCustomerId = localStorage.getItem("customerId");
-//       setCustomerId(storedCustomerId);
-//       try {
-//         const response = await axios.get(
-//           `http://localhost:7000/v1/customers/${storedCustomerId}`
-//         );
-//         const userData = response.data;
-//         setUser({
-//           first_name: userData.first_name,
-//           last_name: userData.last_name,
-//           email: userData.email,
-//           customer_image: userData.customer_image || null,
-//         });
-//       } catch (error) {
-//         toast.error(error.message);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const handleImageChange = (e) => {
-//     const file = e.target.files[0];
-//     setUser((prevUser) => ({
-//       ...prevUser,
-//       customer_image: file,
-//     }));
-//     authContext.setUserImage(file);
-//   };
-
-//   const saveChanges = async () => {
-//     try {
-//       const formData = new FormData();
-//       formData.append("first_name", user.first_name);
-//       formData.append("last_name", user.last_name);
-//       formData.append("email", uwser.email);
-//       if (user.customer_image) {
-//         formData.append("customer_image", user.customer_image);
-//       }
-//       formData.append("password", user.password);
-//       formData.append("confirmPassword", user.confirmPassword);
-
-//       const response = await axios.patch(
-//         `http://localhost:7000/v1/customers/update/${customerId}`,
-//         formData,
-//         {
-//           headers: {
-//             "Content-Type": "multipart/form-data",
-//           },
-//         }
-//       );
-//       toast.success(response.data);
-//     } catch (error) {
-//       toast.error("Couldn't update customer", { error });
-//     }
-//   };
-
-//   return (
-//     <div className="edit-profile">
-//       <div className="contone">
-//         <div
-//           style={{
-//             border: "solid #fff",
-//             margin: "2%",
-//             width: "200px",
-//             height: "200px",
-//             borderRadius: "50%",
-//             overflow: "hidden",
-//             backgroundColor: "#f9e9c8",
-//             position: "relative",
-//           }}
-//         >
-//           {user.customer_image ? (
-//             <img
-//               src={
-//                 user.customer_image instanceof File
-//                   ? URL.createObjectURL(user.customer_image)
-//                   : user.customer_image
-//               }
-//               alt=""
-//               style={{
-//                 width: "100%",
-//                 height: "100%",
-//                 objectFit: "cover",
-//                 cursor: "pointer",
-//                 position: "absolute",
-//               }}
-//               onClick={() => document.getElementById("image-input").click()}
-//             />
-//           ) : (
-//             <img style={{ width: "100%", height: "100%" }} src="" alt="" />
-//           )}
-//           <input
-//             type="file"
-//             id="image-input"
-// style={{
-//   position: "absolute",
-//   top: 0,
-//   left: 0,
-//   width: "100%",
-//   height: "100%",
-//   opacity: 0,
-//   cursor: "pointer",
-// }}
-//             accept="image/*"
-//             onChange={handleImageChange}
-//           />
-//         </div>
-
-//         <div>
-//           <div className="inputs">
-//             <span>First Name</span>
-//             <input
-//               type="text"
-//               value={user.first_name}
-//               onChange={(e) => setUser({ ...user, first_name: e.target.value })}
-//             />
-//           </div>
-//           <div className="inputs">
-//             <span>Last Name</span>
-//             <input
-//               type="text"
-//               value={user.last_name}
-//               onChange={(e) => setUser({ ...user, last_name: e.target.value })}
-//             />
-//           </div>
-//         </div>
-//         <div>
-//           <div className="inputs">
-//             <span>Email</span>
-//             <input
-//               type="text"
-//               value={user.email}
-//               onChange={(e) => setUser({ ...user, email: e.target.value })}
-//             />
-//           </div>
-//           {/* </div> */}
-//           {/* <div> */}
-//           <div className="inputs">
-//             <span>Password</span>
-//             <input
-//               type="password"
-//               value={user.password}
-//               onChange={(e) => setUser({ ...user, password: e.target.value })}
-//             />
-//           </div>
-//         </div>
-//         <div>
-//           <div className="inputs">
-//             <span>Confirm Password</span>
-//             <input
-//               type="password"
-//               value={user.confirmPassword}
-//               onChange={(e) =>
-//                 setUser({ ...user, confirmPassword: e.target.value })
-//               }
-//             />
-//           </div>
-
-//           <div className="inputs">
-//             <div className="save-button">
-// <Button style={{
-//       cursor: "pointer",
-//       backgroundColor: "#590404",
-//       color: "#fff",
-//     }} type="submit" onClick={saveChanges}>
-//                 Save
-//               </Button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <ToastContainer />
-//     </div>
-//   );
-// }
-
-// export default Profil;
-
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Button } from "react-bootstrap";
+// import { Button } from "react-bootstrap";
+import Button from "@mui/material/Button";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthContext } from "../../Components/Logincontext/Logincontext";
@@ -210,7 +9,7 @@ import "./Profile.css";
 
 function Profil() {
   const authContext = useContext(AuthContext);
-  const handleLogout = authContext.logout
+  const handleLogout = authContext.logout;
   const [customer, setCustomer] = useState({
     first_name: "",
     last_name: "",
@@ -237,7 +36,6 @@ function Profil() {
           email: customerData.email,
           customer_image: customerData.customer_image || null,
         });
-
       } catch (error) {
         toast.error(error.message);
       }
@@ -284,125 +82,141 @@ function Profil() {
 
   return (
     <div className="edit-profile">
-      <div
-        style={{
-          border: "solid #fff",
-          margin: "2%",
-          width: "200px",
-          height: "200px",
-          borderRadius: "50%",
-          overflow: "hidden",
-          backgroundColor: "#f9e9c8",
-          position: "relative",
-        }}
-      >
-        {customer.customer_image ? (
-          <img
-            src={
-              customer.customer_image instanceof File
-                ? URL.createObjectURL(customer.customer_image)
-                : customer.customer_image
-            }
-            alt=""
-            style={{
-              width: "100%",
+      <div className="contone">
+        <div
+          style={{
+            border: "solid #fff",
+            margin: "2%",
+            width: "200px",
+            height: "200px",
+            borderRadius: "50%",
+            overflow: "hidden",
+            backgroundColor: "#f9e9c8",
+            position: "relative",
+          }}
+        >
+          {customer.customer_image ? (
+            <img
+              src={
+                customer.customer_image instanceof File
+                  ? URL.createObjectURL(customer.customer_image)
+                  : customer.customer_image
+              }
+              alt=""
+              style={{
+                width: "100%",
                 height: "100%",
                 objectFit: "cover",
                 cursor: "pointer",
                 position: "absolute",
+              }}
+              onClick={() => document.getElementById("image-input").click()}
+            />
+          ) : (
+            <img style={{ width: "100%", height: "100%" }} src="" alt="" />
+          )}
+          <input
+            type="file"
+            id="image-input"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              opacity: 0,
+              cursor: "pointer",
             }}
-            onClick={() => document.getElementById("image-input").click()}
+            accept="image/*"
+            onChange={handleImageChange}
           />
-        ) : (
-          <img style={{ width: "100%", height: "100%" }} src="" alt="" />
-        )}
-        <input
-          type="file"
-          id="image-input"
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            opacity: 0,
-            cursor: "pointer",
-          }}
-          accept="image/*"
-          onChange={handleImageChange}
-        />
-      </div>
-      <div className="inputs">
-        <label>First Name</label>
-        <input
-          type="text"
-          value={customer.first_name}
-          onChange={(e) =>
-            setCustomer({ ...customer, first_name: e.target.value })
-          }
-        />
-      </div>
-      <div className="inputs">
-        <label>Last Name</label>
-        <input
-          type="text"
-          value={customer.last_name}
-          onChange={(e) =>
-            setCustomer({ ...customer, last_name: e.target.value })
-          }
-        />
-      </div>
-      <div className="inputs">
-        <label>Password</label>
-        <input
-          type="password"
-          value={customer.password}
-          onChange={(e) =>
-            setCustomer({ ...customer, password: e.target.value })
-          }
-        />
-      </div>
-      <div className="inputs">
-        <label>New Password</label>
-        <input
-          type="password"
-          value={customer.new_password}
-          onChange={(e) =>
-            setCustomer({ ...customer, new_password: e.target.value })
-          }
-        />
-      </div>
-      <div className="inputs">
-        <label>Email</label>
-        <input
-          type="text"
-          value={customer.email}
-          onChange={(e) => setCustomer({ ...customer, email: e.target.value })}
-        />
-      </div>
-      <div className="save-button">
-        <Button
-          style={{
-            cursor: "pointer",
-            backgroundColor: "#590404",
-            color: "#fff",
-          }}
-          type="submit"
-          onClick={saveChanges}
-        >
-          Save
-        </Button>
-        <Button
-          style={{
-            cursor: "pointer",
-            backgroundColor: "#590404",
-            color: "#fff",
-          }}
-          // type="submit"
-          onClick={handleLogout}
-        >
-          Logout
-        </Button>
+        </div>
+
+        <div>
+          <div className="inputs">
+            <label>First Name</label>
+            <input
+              type="text"
+              value={customer.first_name}
+              onChange={(e) =>
+                setCustomer({ ...customer, first_name: e.target.value })
+              }
+            />
+          </div>
+          <div className="inputs">
+            <label>Last Name</label>
+            <input
+              type="text"
+              value={customer.last_name}
+              onChange={(e) =>
+                setCustomer({ ...customer, last_name: e.target.value })
+              }
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="inputs">
+            <label>Email</label>
+            <input
+              type="text"
+              value={customer.email}
+              onChange={(e) =>
+                setCustomer({ ...customer, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="inputs">
+            <label>Password</label>
+            <input
+              type="password"
+              value={customer.password}
+              onChange={(e) =>
+                setCustomer({ ...customer, password: e.target.value })
+              }
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="inputs">
+            <label>New Password</label>
+            <input
+              type="password"
+              value={customer.new_password}
+              onChange={(e) =>
+                setCustomer({ ...customer, new_password: e.target.value })
+              }
+            />
+          </div>
+
+          <div className="inputs">
+            <div className="save-button">
+              <Button
+                style={{
+                  cursor: "pointer",
+                  backgroundColor: "#590404",
+                  color: "#fff",
+                }}
+                type="submit"
+                onClick={saveChanges}
+              >
+                Save
+              </Button>
+              <Button
+                style={{
+                  cursor: "pointer",
+                  backgroundColor: "#590404",
+                  color: "#fff",
+                }}
+                // type="submit"
+                onClick={handleLogout}
+              >
+                Logout
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
       <ToastContainer />
     </div>
